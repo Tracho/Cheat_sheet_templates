8ec5b6faeca3bdfde2fbbbf18ec505e78acf0bf4
IgorShumsky <Tracho2013@gmail.com>
2025-03-13 19:14:55 +0200

    first

---
+++ #src/js/modules/bnt_copy.js
@@ -0,0 +1,29 @@
+function bnt_copy() { 
+  let bnt_copy = document.querySelectorAll("._bnt_copy");
+  let input_view_code = document.querySelectorAll("._input_view_code");
+
+  bnt_copy.forEach((btn, index) => {
+    btn.addEventListener("click", () => { 
+      let input = input_view_code[index];
+      input.select();
+      document.execCommand("copy");
+
+      // Создаем Tooltip
+      let tooltip = new bootstrap.Tooltip(btn, {
+        title: "Сopied!", // Текст всплывающей подсказки
+        placement: "top", // Расположение
+        trigger: "manual" // Управляем вручную
+      });
+
+      tooltip.show(); // Показываем Tooltip
+
+      // Закрываем Tooltip через 0,5 секунды
+      setTimeout(() => {
+        tooltip.hide();
+      }, 1000);
+    });
+  });
+}
+
+
+export default bnt_copy;
---
+++ #src/js/modules/btn_view_code.js
@@ -0,0 +1,60 @@
+// import json from '../json.json';
+let data = null;
+ async function loadJson() {
+  let url = document.location.protocol   + '/json/json.json'; 
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Ошибка сети: ' + response.statusText);
+    }
+     data = await response.json();
+    
+    return data;
+    // Здесь можно работать с данными из JSON-файла
+  } catch (error) {
+    console.error('Ошибка при загрузке JSON:', error);
+  }
+}loadJson();
+(async () => {
+  await loadJson();
+  console.log(data);
+})();
+ 
+
+function listener_view_code(btn){
+  btn.addEventListener("click", function() { 
+    let objec_key = btn.dataset.objec_key; 
+    let view_code_key = btn.dataset.view_code_key;
+
+    (async () => {  
+      await loadJson(); 
+      let keys = Object.keys(data[objec_key][view_code_key]);
+      let this_key = data[objec_key][view_code_key];
+      console.log('Глобальная переменная data:', data);
+      // console.log(keys)
+      let input_view_code = document.querySelectorAll("._input_view_code");
+      input_view_code.forEach((input, index) => {
+        let d_inp = input.dataset.input_view_code;
+        let full_data = this_key[d_inp]; 
+        input.value = full_data;  
+      });
+
+    })(); 
+  });
+}
+ 
+function btn_view_code() { 
+  let _mini_btn_view_code = document.querySelectorAll('._render_section ._mini_btn_view_code');
+  if(_mini_btn_view_code.length > 0){
+    _mini_btn_view_code.forEach((btn) => {
+      listener_view_code(btn)
+    });  // Запускаем событие для мини-кнопок, если они есть в шаблоне.
+  }console.log(_mini_btn_view_code)
+
+  let btn_view_code = document.querySelectorAll("._btn_view_code");  
+  btn_view_code.forEach((btn) => {
+    listener_view_code(btn)
+  });
+} 
+
+export default btn_view_code;
